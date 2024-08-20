5b482925ca1c2b3b9be240b814c7560f8e8e0a5b
pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-08-20 17:12:17 -0400

    fix sql prepare check

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -3,13 +3,15 @@ use actix_web::{
     web,
     HttpResponse,
 };
+use tracing::info;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
     subscription_token: String,
 }
 
-#[tracing::instrument(name = "Confirm a pending subscriber", skip(_parameters))]
-pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters))]
+pub async fn confirm(parameters: web::Query<Parameters>) -> HttpResponse {
+    info!("Params are {}", parameters.subscription_token);
     HttpResponse::Ok().finish()
 }
