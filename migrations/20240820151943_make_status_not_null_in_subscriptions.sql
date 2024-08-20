53e4bffa7ffdca002419a22300337ae8dc558ef0
pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-08-20 11:22:39 -0400

    feat(schema): back-fill status column in older rows before making it mandatory

---
+++ migrations/20240820151943_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,10 @@
+-- Sqlx does not automatically enforce atomic tranasctions
+-- manually wrap it in a transaction
+
+BEGIN;
+-- Backfill `status` for historical entries
+UPDATE subscriptions
+SET status = 'confirmed' WHERE status IS NULL;
+
+-- Make `status` mandatory
+ALTER TABLE subscriptions ALTER COLUMN status SET NOT NULL; COMMIT;
