0d47cb1bfd7798a6c3ec211c87b30460a87838a4
Pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-09-26 17:21:05 -0400

    fix: updates necessary due to quick package change

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -23,20 +23,22 @@ impl AsRef<str> for SubscriberEmail {
 
 #[cfg(test)]
 mod tests {
+    use super::SubscriberEmail;
     use claims::assert_err;
     use fake::{faker::internet::en::SafeEmail, Fake};
-    use quickcheck::Gen;
     use quickcheck_macros::quickcheck;
-
-    use super::SubscriberEmail;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
     // Both `Clone` and `Debug` are required by `quickcheck`
     #[derive(Debug, Clone)]
     struct ValidEmailFixture(pub String);
 
     impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary(g: &mut Gen) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+
             Self(email)
         }
     }
